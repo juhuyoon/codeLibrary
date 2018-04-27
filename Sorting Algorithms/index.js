81b3a3eebb9926b0d475a59651d011e23d35b9d4
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-04-27 15:32:00 -0400

    additional sort

--- Sorting Algorithms/index.js
+++ Sorting Algorithms/index.js
@@ -104,3 +104,89 @@
                     arr[i] = arr[j];
                     arr[j] = temp;
                 }
+
+ 5)Merge Sort
+        //Breaking down arrays into smaller pieces and compare the individual items in each of those pieces, then merge all together after comparisons.
+        function mergeSort(arr){
+            var len = arr.length;
+            if(len <2)
+               return arr;
+            var mid = Math.floor(len/2),
+                left = arr.slice(0,mid),
+                right =arr.slice(mid);
+            //send left and right to the mergeSort to broke it down into pieces
+            //then merge those
+            return merge(mergeSort(left),mergeSort(right));
+         }
+                  
+            function merge(left, right){
+                var result = [],
+                    lLen = left.length,
+                    rLen = right.length,
+                    l = 0,
+                    r = 0;
+                while(l < lLen && r < rLen){
+                if(left[l] < right[r]){
+                    result.push(left[l++]);
+                }
+                else{
+                    result.push(right[r++]);
+                }
+                }  
+                //remaining part needs to be addred to the result
+                return result.concat(left.slice(l)).concat(right.slice(r));
+            }
+
+ 6) Heap Sort
+        //https://www.geeksforgeeks.org/heap-sort/
+        //Comparison based sorting technique             
+            function heapSort(arr){
+                var len = arr.length,
+                    end = len-1;
+            
+                heapify(arr, len);
+                
+                while(end > 0){
+                swap(arr, end--, 0);
+                siftDown(arr, 0, end);
+                }
+                return arr;
+            }
+
+                function heapify(arr, len){
+                    // break the array into root + two sides, to create tree (heap)
+                    var mid = Math.floor((len-2)/2);
+                    while(mid >= 0){
+                    siftDown(arr, mid--, len-1);    
+                }
+                }
+
+                    function siftDown(arr, start, end){
+                        var root = start,
+                            child = root*2 + 1,
+                            toSwap = root;
+                        while(child <= end){
+                        if(arr[toSwap] < arr[child]){
+                            swap(arr, toSwap, child);
+                        }
+                        if(child+1 <= end && arr[toSwap] < arr[child+1]){
+                            swap(arr, toSwap, child+1)
+                        }
+                        if(toSwap != root){
+                            swap(arr, root, toSwap);
+                            root = toSwap;
+                        }
+                        else{
+                            return; 
+                        }
+                        toSwap = root;
+                        child = root*2+1
+                    }
+                    }
+                                            
+                        function swap(arr, i, j){
+                            var temp = arr[i];
+                            arr[i] = arr[j];
+                            arr[j] = temp;
+                        }
+                                
