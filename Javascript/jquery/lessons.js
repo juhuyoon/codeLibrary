6f66bc3a31ec0d50075fb4c346db92e253ebffd4
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-08-01 17:59:19 -0400

    jquery

---
+++ Javascript/jquery/lessons.js
@@ -0,0 +1,5 @@
+//https://medium.com/front-end-hacking/how-to-use-a-javascript-library-jquery-75d0b6d50b5a
+//http://blondiebits.herokuapp.com/#/4
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/script.js"></script>
