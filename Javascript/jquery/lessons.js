1bc2541bd2237bbf072e9b5b9da1b0988a14708b
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-08-02 00:45:47 -0400

    jquery

--- Javascript/jquery/lessons.js
+++ Javascript/jquery/lessons.js
@@ -3,3 +3,5 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="js/script.js"></script>
+
+//All we need to do is write the JavaScript so that when we click the button, the message is revealed so we’ll go to our script.js and write a function. To create a function, we will write the keyword function and then the name of the function - here, it’s revealMessage. If we needed to add parameters to the function, then we would put those in between the parentheses, but we don't need to worry about that right now so we won't put anything inside. Then, we can add the body of our function - what it will actually do when revealMessage() is called. Inside our function, we'll access the variable document (aka our HTML document) and then get the element with the id hiddenMessage (our paragraph we created before). Next, we will access this element’s style, in other words it’s CSS, and set its display property to the value 'block'.
