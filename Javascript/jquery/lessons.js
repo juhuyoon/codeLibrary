1758376239c29e91d36baef230760d58399f94c7
Junghoon Yoon <juhuyoon@yahoo.com>
2018-08-04 22:15:17 -0400

    additional jquery

--- Javascript/jquery/lessons.js
+++ Javascript/jquery/lessons.js
@@ -1,6 +1,7 @@
 //https://medium.com/front-end-hacking/how-to-use-a-javascript-library-jquery-75d0b6d50b5a
 //http://blondiebits.herokuapp.com/#/4
 //https://medium.com/@jaeger.rob/jquery-selectors-the-absolute-basics-d781500c722c
+//http://learn.jquery.com/using-jquery-core/selecting-elements/
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -11,7 +12,7 @@
 
 <h1>Hello jQuery!</h1>
 <p class="intro-content">Let's use some selectors.</p>
-Let’s get the heading element using jQuery. Here we can look at vanilla JavaScript and jQuery together.
+//Let’s get the heading element using jQuery. Here we can look at vanilla JavaScript and jQuery together.
 
 // Vanilla JavaScript
 document.querySelector("h1");
@@ -27,3 +28,6 @@ document.querySelector(".intro-content");
 // jQuery
 $(".intro-content");
 //Again, really the only difference is the $. The CSS selector (in the quotes) is the same between vanilla JavaScript and jQuery!
+//The JavaScript Element object does not know what .text() means. It only knows what innerText means.
+//The moral of the story: when using jQuery, use only jQuery methods to manipulate jQuery objects.
+
