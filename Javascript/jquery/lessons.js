d3397d6fc1b455303ab6cf3dfe0683691664732e
Junghoon Yoon <juhuyoon@yahoo.com>
2018-08-04 20:49:10 -0400

    jquery updates

--- Javascript/jquery/lessons.js
+++ Javascript/jquery/lessons.js
@@ -1,54 +1,29 @@
 //https://medium.com/front-end-hacking/how-to-use-a-javascript-library-jquery-75d0b6d50b5a
 //http://blondiebits.herokuapp.com/#/4
+//https://medium.com/@jaeger.rob/jquery-selectors-the-absolute-basics-d781500c722c
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="js/script.js"></script>
 
-//All we need to do is write the JavaScript so that when we click the button, the message is revealed so we’ll go to our script.js and write a function. To create a function, we will write the keyword function and then the name of the function - here, it’s revealMessage. If we needed to add parameters to the function, then we would put those in between the parentheses, but we don't need to worry about that right now so we won't put anything inside. Then, we can add the body of our function - what it will actually do when revealMessage() is called. Inside our function, we'll access the variable document (aka our HTML document) and then get the element with the id hiddenMessage (our paragraph we created before). Next, we will access this element’s style, in other words it’s CSS, and set its display property to the value 'block'.
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>JavaScript Content!</title>
-</head>
-<body>
+//Transitioning to jQuery
+//Now for the jQuery. Here is that same HTML snippet again.
 
-  <h1>Drink Selection!</h1>
-  <hr>
+<h1>Hello jQuery!</h1>
+<p class="intro-content">Let's use some selectors.</p>
+Let’s get the heading element using jQuery. Here we can look at vanilla JavaScript and jQuery together.
 
-  <!-- Here we create a generic drink-options div -->
-  <div id="drink-options"></div>
-
-  <script type="text/javascript">
-
-    // Array holds all of the drinks available
-    var drinkList = [
-      "Coffee: $5",
-      "Espresso: $7",
-      "Cappuccino: $6",
-      "Latte: $4",
-      "Tea: $3",
-      "Ice Coffee: $6",
-      "Ice Espresso: $8",
-      "Ice Latte: $6",
-      "Ice Tea: $4"
-    ];
-
-    // 1. Create code that "grabs" the div with the matching id (#drink-options);
-    // ...
+// Vanilla JavaScript
+document.querySelector("h1");
+// jQuery
+$("h1");
 
 
-    // ...
+//These two pieces of code have the same purpose! They look in the document for an h1 element and grab that element. Virtually the only difference is the dollar sign ($).
+//Here is the matching class selector for jQuery:
 
-
-    // 2. Create a for loop that creates HTML content of all the drinks using JavaScript.
-    // HINT: You will need to use each of the following methods: createElement, innerHTML, appendChild
-    // ...
-
-
-    // ...
-
-  </script>
-
-</body>
-</html>
+// Vanilla JavaScript
+document.querySelector(".intro-content");
+// jQuery
+$(".intro-content");
+//Again, really the only difference is the $. The CSS selector (in the quotes) is the same between vanilla JavaScript and jQuery!
