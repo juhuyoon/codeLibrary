3cb6334bb289918ffaee879d0a3b221311681e6a
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-08-09 17:48:01 -0400

    more

--- GATech_prac/Tech_Interview_JS.js
+++ GATech_prac/Tech_Interview_JS.js
@@ -34,3 +34,31 @@ inner func:  self.foo = bar
     })();
 
 //result : 1, 4, 3, 2
+
+//What will the code below output to the console and why?
+var arr1 = "john".split('');
+var arr2 = arr1.reverse();
+var arr3 = "jones".split('');
+arr2.push(arr3);
+console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1));
+console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));
+
+//result: 
+"array 1: length=5 last=j,o,n,e,s";
+"array 2: length=5 last=j,o,n,e,s";
+
+//What will the code below output to the console and why?
+console.log(1 + "2" + "2");
+console.log(1 + +"2" + "2");
+console.log(1 + -"1" + "2");
+console.log(+"1" + "1" + "2");
+console.log("A" - "B" + "2");
+console.log("A" - "B" + 2);
+
+//result: 
+"122"
+"32"
+"02"
+"112"
+"NaN2"
+NaN
