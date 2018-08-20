f517b1799c1c5515ec4ec9870e46933cfdf12a48
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-08-19 21:58:33 -0400

    javastudies

---
+++ Java/Introduction.java
@@ -0,0 +1,19 @@
+// http://greggordon.org/java/tij4/solutions.htm 
+//Soln Guide    
+
+//1) Everything is an Object
+//2) A program is a bunch of objects telling each other what to do by sending messages.
+//3) Each object has its own memory made up of other objects. 
+//4) Every object has a type.
+//5) All objects of a particular type can receive the same messages.
+
+//Class: Creating abstract data typpes is a fundamental concept in object-oriented programming. 
+//Since a class describes a set of objects, a class is really a data type. 
+    //Once a class is established, can make as many objects of that class as you like and then manipulate those objects. 
+    //A major challenge of object-oriented programming is to create a one-to-one mapping between elements int he problem space and objects in the solution space. 
+
+    //To make an object useful, requests must be made and these are defined by an interface. 
+    //Interface determines the requests that you can make for a particular object. 
+
+//An object provides services: 
+    
