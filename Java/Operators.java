c71acdfc988876cbc1207e01ca7ea6dc66ea4d1a
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-08-31 21:04:48 -0400

    exs

--- Java/Operators.java
+++ Java/Operators.java
@@ -119,3 +119,19 @@ obj2.f = 20.0
 
 
 //ex3): Create a class containing a float and use it to demonstrate aliasing during method calls.
+class Box {
+	float a;
+}
+
+public class PassObject2 {
+	static void f(Box y) {
+		y.a = 2.71828f;
+	}
+	public static void main(String[] args) {
+		Box x = new Box();
+		x.a = 3.1416f;
+		print("1: x.a = " + x.a);
+		f(x);	
+		print("2: x.a = " + x.a);
+	}
+}
