4d39c702cb33b7fc661da4dc28881d3d83c3925a
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-09-10 19:40:48 -0400

    gene quizzes need more prac

--- Java/coursera/solving_problems_with_software/week_two/finding_gene.java
+++ Java/coursera/solving_problems_with_software/week_two/finding_gene.java
@@ -1,3 +1,5 @@
+import jdk.nashorn.api.scripting.URLReader;
+
 // 1. Create a new Java project named StringsFirstAssignments. You can put all the classes for this programming exercise in this project.
 
 // 2. Create a new Java Class named Part1. The following methods go in this class.
@@ -48,3 +50,152 @@ public void testSimpleGene() {
         Part1 gene = new Part1();
         gene.testSimpleGene();
     }
+}
+
+
+// Part 2: Finding a Gene - Using the Simplified Algorithm Reorganized
+    //determine if a DNA strand has a gene in it by using the simplified algorithm from the lesson. 
+    //Specifically, you should do the following:
+
+// 1. Create a new Java Class named Part2 in the StringsFirstAssignments project.
+
+// 2. Copy and paste the two methods findSimpleGene and testSimpleGene from the Part1 class into the Part2 class.
+
+// 3. The method findSimpleGene has one parameter for the DNA string named dna. Modify findSimpleGene to add two additional parameters, one named startCodon for the start codon and one named stopCodon for the stop codon. What additional changes do you need to make for the program to compile? After making all changes, run your program to check that you get the same output as before.
+
+// 4. Modify the findSimpleGene method to work with DNA strings that are either all uppercase letters such as “ATGGGTTAAGTC” or all lowercase letters such as “gatgctataat”. Calling findSimpleGene with “ATGGGTTAAGTC” should return the answer with uppercase letters, the gene “ATGGGTTAA”, and calling findSimpleGene with “gatgctataat” should return the answer with lowercase letters, the gene “atgctataa”. HINT: there are two string methods toUpperCase() and toLowerCase(). If dna is the string “ATGTAA” then dna.toLowerCase() results in the string “atgtaa”.
+
+public class Part2 {
+    public String findSimpleGene(String dna, String startCodon, String stopCodon) {
+        String result = "";
+        if(Character.isUpperCase(dna.charAt(0))) {
+            startCodon = startCodon.toUpperCase();
+            stopCodon = stopCodon.toLowerCase();
+        } else {
+            startCodon = startCodon.toLowerCase();
+            stopCodon = stopCodon.toLowerCase();
+        }
+        int start = dna.indexOf(startCodon);
+        if (start == -1) {
+            return "";
+        }
+        int stop = dna.indexOf(stopCodon, start);
+        if (stop == -1) {
+            return "";
+        }
+        if ((stop - start) % 3 == 0) {
+            return dna.substring(start, stop + 3);
+        } else {
+            return "";
+        }
+    }
+
+    public void testSimpleGene() {
+        String a = "AAATGCCCTAACTAGATTAAGAAACC";
+        String ap = "CCAATGCAGCGATAC";
+        String apa = "CTAATCCGGATCCGA";
+        String app = "CCAGCATGCCAGTCAGCTAACAG";
+        String appa = "CCAGCATGCCAGTAGCTAACAG";
+
+        System.out.println("The string is: " + a + ". The Gene is: " + findSimpleGene(a, "ATG", "TAA"));
+        System.out.println("The string is: " + ap + ". The Gene is: " + findSimpleGene(ap, "ATG", "TAA"));
+        System.out.println("The string is: " + apa + ". The Gene is: " + findSimpleGene(apa, "ATG", "TAA"));
+        System.out.println("The string is: " + app + ". The Gene is: " + findSimpleGene(app, "ATG", "TAA"));
+        System.out.println("The string is: " + appa + ". The Gene is: " + findSimpleGene(appa, "ATG", "TAA"));
+    }
+
+    public static void main(String[] args) {
+        Part2 gene = new Part2();
+        gene.testSimpleGene();
+    }
+}
+
+// Part 3: Problem Solving with Strings
+// This assignment will give you additional practice using String methods. You will write two methods to solve some problems using strings and a third method to test these two methods.
+
+// Specifically, you should do the following:
+
+// 1. Create a new Java Class named Part3 in the StringsFirstAssignments project. Put the following methods in this class.
+
+// 2. Write the method named twoOccurrences that has two String parameters named stringa and stringb. This method returns true if stringa appears at least twice in stringb, otherwise it returns false. For example, the call twoOccurrences(“by”, “A story by Abby Long”) returns true as there are two occurrences of “by”, the call twoOccurrences(“a”, “banana”) returns true as there are three occurrences of “a” so “a” occurs at least twice, and the call twoOccurrences(“atg”, “ctgtatgta”) returns false as there is only one occurence of “atg”.
+
+// 3. Write the void method named testing that has no parameters. This method should call twoOccurrences on several pairs of strings and print the strings and the result of calling twoOccurrences (true or false) for each pair. Be sure to test examples that should result in true and examples that should result in false.
+
+// 4. Write the method named lastPart that has two String parameters named stringa and stringb. This method finds the first occurrence of stringa in stringb, and returns the part of stringb that follows stringa. If stringa does not occur in stringb, then return stringb. For example, the call lastPart(“an”, “banana”) returns the string “ana”, the part of the string after the first “an”. The call lastPart(“zoo”, “forest”) returns the string “forest” since “zoo” does not appear in that word.
+
+// 5. Add code to the method testing to call the method lastPart with several pairs of strings. For each call print the strings passed in and the result. For example, the output for the two calls above might be:
+
+// The part of the string after an in banana is ana.
+// The part of the string after zoo in forest is forest.
+
+public class Part3 {
+    public boolean twoOccurrences(String stringa, String stringb) {
+        int index1 = stringb.indexOf(stringa);
+        if(index1 != -1) {
+            int index2 = stringb.indexOf(stringa, index1+1);
+            if(index2 != -1) {
+                return true;
+            }
+        } else {
+            return false;
+        }
+        return false;
+    }
+    public void testing() {
+        System.out.println(twoOccurrences("by", "A story by Abby Long"));
+        System.out.println(twoOccurrences("a", "banana"));
+        System.out.println(twoOccurrences("aatg", "ctgtatgta"));
+        System.out.println(twoOccurrences("an", "banana"));
+        System.out.println(twoOccurrences("zoo", "forest"));
+        System.out.println(twoOccurrences("zoo", "farzookeeper"));
+    }
+
+    public String lastPart(String stringa, String stringb) {
+        if(stringb.indexOf(stringa) != -1) {
+            int index = stringb.indexOf(stringa);
+            return stringb.substring(index+stringa.length(), stringb.length());
+        } else {
+            return stringb;
+        }
+    }
+
+    public static void main() {
+        Part3 howMany = new Part3();
+        howMany.testing();
+    }
+}
+
+// //Part 4: Finding Web Links
+// Write a program that reads the lines from the file at this URL location, http://www.dukelearntoprogram.com/course2/data/manylinks.html, and prints each URL on the page that is a link to youtube.com. Assume that a link to youtube.com has no spaces in it and would be in the format (where [stuff] represents characters that are not verbatim): “http:[stuff]youtube.com[stuff]”
+
+// Here are suggestions to get started.
+
+// 1. Create a new Java Class named Part4 in the StringsFirstAssignments project and put your code in that class.
+
+// 2. Use URLResource to read the file at http://www.dukelearntoprogram.com/course2/data/manylinks.html word by word.
+
+// 3. For each word, check to see if “youtube.com” is in it. If it is, find the double quote to the left and right of the occurrence of “youtube.com” to identify the beginning and end of the URL. Note, the double quotation mark is a special character in Java. To look for a double quote, look for (\”), since the backslash (\) character indicates we want the literal quotation marks (“) and not the Java character. The string you search for would be written “\”” for one double quotation mark.
+
+// 4. In addition to the String method indexOf(x, num), you might want to consider using the String method lastIndexOf(s, num) that can be used with two parameters s and num. The parameter s is the string or character to look for, and num is the last position in the string to look for it. This method returns the last match from the start of the string up to the num position, so it is a good option for finding the opening quotation mark of a string searching backward from “youtube.com.” More information on String methods can be found in the Java documentation for Strings: http://docs.oracle.com/javase/7/docs/api/java/lang/String.html.
+
+public class Part4{
+    public void printUrls(String url) {
+    URLResource myurl = new URLResource(url);
+    for(String word : myurl.words()) {
+        if(word.toLowerCase().indexOf("youtube.com") != -1) {
+            int quoteIndex = word.indexOf("\"");
+            int lastQuoteIndex = word.indexOf("\"", quoteIndex+1);
+            System.out.println(word.substring(quoteIndex+1, lastQuoteIndex));
+        }
+    }
+}
+    public void testUrl() {
+        printUrls("https://www.dukelearntoprogram.com/course2/data/manylinks.html");
+    }
+
+    public static void main() {
+        Part4 url = new Part4();
+        url.testUrl();
+    }
+
+}
---
+++ Java/coursera/solving_problems_with_software/week_two/quizzes.java
@@ -0,0 +1,37 @@
+// 1) Consider the assignment that goes with this lesson and its algorithm for finding a gene with stop codon TAA.
+// Consider the following DNA string.
+// “AAATGCCCTAACTAGATTAAGAAACC”
+// Which one of the following is the gene returned using that algorithm?\
+
+//result: "ATGCCCTAA"
+
+// 2) Which one of the following replaces the String variable dna with the lowercase version of this string?
+//result: 
+dna=dna.toLowerCase();
+// 3) After adding two additional parameters to findSimpleGene for the startCodon and stopCodon, which of the following is another change that must be made for the program to compile?
+//Two arguments must be added to the call to findSimpleGene.
+
+// 4) Suppose that stringa is in stringb at position pos. Which one of the following returns the part of stringb that comes after stringa in the method lastPart?
+
+//result: 
+return stringb.substring(pos+stringa.length());
+
+// 5) In the method twoOccurrences, suppose pos is the value of the first occurrence of stringa when stringa is found. Which of the following lines of code assigns pos2 to the second occurrence of stringa if there is a second occurrence?
+
+//result: 
+pos2 = stringb.indexOf(stringa, pos+1);
+
+//6) Consider assignment 2 for this lesson. Consider the code to find and print all the YouTube URL links.
+URLResource file = new  URLResource("http://someURL");
+   	for (String item : file.words()) {
+       	   String itemLower = item.toLowerCase();
+       	   int pos = itemLower.indexOf("youtube.com");
+       	   if (pos != -1) {
+           	MISSING CODE
+               }
+       }
+       
+//result: which one of the following is the missing code to find and print all the Youtube URLs?
+int beg = item.lastIndexOf("\"",pos);
+int end = item.indexOf("\"", pos+1);
+System.out.println(item.substring(beg+1,end));
