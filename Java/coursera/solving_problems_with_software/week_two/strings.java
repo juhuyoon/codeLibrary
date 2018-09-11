d345768e210f1342fad549e748fcaa05dc724350
Junghoon Yoon <juhuyoon@yahoo.com>
2018-09-11 19:07:29 -0400

    psuedocoding

--- Java/coursera/solving_problems_with_software/week_two/strings.java
+++ Java/coursera/solving_problems_with_software/week_two/strings.java
@@ -18,64 +18,96 @@
             //Between two indices, using a built-in method in the String class
 
            
-    String s = "dukeprogramming" ; //String literals in quotes
-    String x = s.substring(4,7); //counts down, substring index starts at 4(inclusive), ends at 7(exclusive)
-    String z = s.length() //counts the # of characters in the String
-    String a = s.indexOf("program") Result = 4; //Find one string within another, return the starting index, starting with where "program" is. 
-    String b = s.indexOf("g") //Result = 7    
-    String f = s.indexOf('f') //If the letter does not exist, it will return -1
-    String c = s.indexOf("g", 8) //can give another parameter where to start searching, so it would return value 14
-    String y = s.startsWith("duke") //true
-    String q = s.endsWith("king") //false
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-public class FindGeneSimpleandTest {
-    public String findGeneSimple(String dna) {
-        //start codon is "ATG"
-        //stop codon is "TAA"
-        String result = "" ;
-        int startIndex = dna.indexOf("ATG"); //returns location of where it starts
-        if(startIndex == -1) //no ATG {
-            return "";
-        };
-        int stopIndex = dna.indexOf("TAA", startIndex+3); //returns location of where it ends
-        if(stopIndex == -1) {
-           // No TAA
-           return ""; 
+            String s = "dukeprogramming" ; //String literals in quotes
+            String x = s.substring(4,7); //counts down, substring index starts at 4(inclusive), ends at 7(exclusive)
+            String z = s.length() //counts the # of characters in the String
+            String a = s.indexOf("program") Result = 4; //Find one string within another, return the starting index, starting with where "program" is. 
+            String b = s.indexOf("g") //Result = 7    
+            String f = s.indexOf('f') //If the letter does not exist, it will return -1
+            String c = s.indexOf("g", 8) //can give another parameter where to start searching, so it would return value 14
+            String y = s.startsWith("duke") //true
+            String q = s.endsWith("king") //false
+        
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        public class FindGeneSimpleandTest {
+            public String findGeneSimple(String dna) {
+                //start codon is "ATG"
+                //stop codon is "TAA"
+                String result = "" ;
+                int startIndex = dna.indexOf("ATG"); //returns location of where it starts
+                if(startIndex == -1) //no ATG {
+                    return "";
+                };
+                int stopIndex = dna.indexOf("TAA", startIndex+3); //returns location of where it ends
+                if(stopIndex == -1) {
+                   // No TAA
+                   return ""; 
+                }
+        
+                result = dna.substring(startIndex, stopIndex+3);
+                
+                return result;
+            }
+            
+            public void testFindGeneSimple() {
+                String dna = "AATGCGTAATATGGT";
+                System.out.println("DNA strand is " + dna);
+                String gene = findGeneSimple(dna);
+                System.out.println("Gene is " + gene);
+        
+                dna = "AATGCTAGGGTAATATGGT";
+                System.out.println("DNA strand is " + dna);
+                gene = findGeneSimple(dna);
+                System.out.println("Gene is " + gene);
+                
+                dna = "TTATAA";
+                //dna = "ATGTAA";
+                System.out.println("DNA strand is " + dna);
+                gene = findGeneSimple(dna);
+                System.out.println("Gene is " + gene);
+            }
         }
-
-        result = dna.substring(startIndex, stopIndex+3);
         
-        return result;
-    }
-    
-    public void testFindGeneSimple() {
-        String dna = "AATGCGTAATATGGT";
-        System.out.println("DNA strand is " + dna);
-        String gene = findGeneSimple(dna);
-        System.out.println("Gene is " + gene);
-
-        dna = "AATGCTAGGGTAATATGGT";
-        System.out.println("DNA strand is " + dna);
-        gene = findGeneSimple(dna);
-        System.out.println("Gene is " + gene);
+        //Can use 'mod' to find multiples of 3s and other math operators
+        (a-b) % 3 == 0 
         
-        dna = "TTATAA";
-        //dna = "ATGTAA";
-        System.out.println("DNA strand is " + dna);
-        gene = findGeneSimple(dna);
-        System.out.println("Gene is " + gene);
-    }
-}
+        //Two Different types of numbers
+            //int represents integers = -2, -1, 0, 1, 2
+            //double represents real numbers like floating points = 1.2, 3.4
+            //Arithmetic integers always yields an integer so 5/2 = 2
+                                                            //100 / 3 * 4 = 132
+            
+        //Orders of Operations (precedence & associativity)
 
-//Can use 'mod' to find multiples of 3s and other math operators
-(a-b) % 3 == 0 
+//Searching for many genes
+    //use the power of looping in using iterables to access data and information
+            //Use these techniques to find all genes in DNA
+            //Algorithms for finding one gene and repeat
+                //How many is enough for general cases to check for Start and Stop codons?
+                    //Cannot pick any particular number
+                    //Need algorithm to repeat checks
+    //Storing immediate results
+    //Lets you process results
+            //Lets you filter results
+            //Iterable you add to with code
+                //Repetition will turn into loops
+                    //Figure out WHAT to loop over
+                        //Using both For and While loops;
+    //While Loops
+        //complements of for loop with iterables
 
-//Two Different types of numbers
-    //int represents integers = -2, -1, 0, 1, 2
-    //double represents real numbers like floating points = 1.2, 3.4
-    //Arithmetic integers always yields an integer so 5/2 = 2
-                                                    //100 / 3 * 4 = 132
-    
-//Orders of Operations (precedence & associativity)
+    //StorageResource 
+        //adding values, counting them, filtering them, preview of using arrays
+    //Short-circuited boolean evaluation
+        //Helps in developing good coding practices [architectures]
+
+//New Generalization:
+        //1) Find first occurrence of "ATG" and call its index startIndex
+        //2) Find the "TAA" starting from (startIndex + 3), call this result currIndex;
+
+            //Can make this repetitive! As long as...
+                //3) Check if(currIndex - startIndex) is a multiple of 3
+                //4) If not, update currIndex to the index of the next "TAA", starting from (currIndex + 1)
+                //5) Check if (currIndex - startIndex) is a multiple of 3
+                //6) If so, text between startIndex and currIndex + 3 is the answer
