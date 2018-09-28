37945641eb0f0723cf6135578eb74baf730cf79e
Junghoon Yoon <juhuyoon@yahoo.com>
2018-09-27 20:02:37 -0400

    beginnings of gray_scale_converter

---
+++ Java/coursera/solving_problems_with_software/week_four/grayscale.java
@@ -0,0 +1,55 @@
+//converting many images to grayscale
+//Save the converted, new images as files
+//Add "grey-" prefix to original file name
+
+//1) Ask user to pick files & iterate over them
+//2) Convert one image to grayscale
+//3) Combine and convert many of them
+//4) Save the results
+
+
+//Psuedocoding:
+    //1) Start with an image (colorImage)
+    //2) Make a blank image of the same size (whiteImage)
+    //3) For each pixel in the whiteImage
+        //3a) Look at the corresponding pixel in whiteImage (inPixel)
+        //3b) Compute the inPixel's R + G + B
+        //3c) Divide the sum by 3 (call it average)
+        //3d) Set the pixel's red to average
+        //3e) Set the pixel's green to average
+        //3f) Set the pixel's blue to average
+    //4) outImage will be the answer, and repeat for all images
+
+    //going through the code
+import edu.duke.*;
+import java.io.*;
+
+public class GrayScaleConverter {
+	//I started with the image I wanted (inImage)
+	public ImageResource makeGray(ImageResource inImage) {
+		//I made a blank image of the same size
+		ImageResource outImage = new ImageResource(inImage.getWidth(), inImage.getHeight());
+		//for each pixel in outImage
+		for (Pixel pixel: outImage.pixels()) {
+			//look at the corresponding pixel in inImage
+			Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
+			//compute inPixel's red + inPixel's blue + inPixel's green
+			//divide that sum by 3 (call it average)
+			int average = (inPixel.getRed() + inPixel.getBlue() + inPixel.getGreen())/3;
+			//set pixel's red to average
+			pixel.setRed(average);
+			//set pixel's green to average
+			pixel.setGreen(average);
+			//set pixel's blue to average
+			pixel.setBlue(average);
+		}
+		//outImage is your answer
+		return outImage;
+	}
+
+	public void testGray() {
+		ImageResource ir = new ImageResource();
+		ImageResource gray = makeGray(ir);
+		gray.draw();
+	}
+}
