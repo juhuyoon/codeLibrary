c892090929697d047c5574bb341036b79a111f8e
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-09-28 20:53:12 -0400

    gray scales

--- Java/coursera/solving_problems_with_software/week_four/grayscale.java
+++ Java/coursera/solving_problems_with_software/week_four/grayscale.java
@@ -25,31 +25,56 @@ import edu.duke.*;
 import java.io.*;
 
 public class GrayScaleConverter {
-	//I started with the image I wanted (inImage)
+	// I started with the image I wanted (inImage)
 	public ImageResource makeGray(ImageResource inImage) {
-		//I made a blank image of the same size
+		// I made a blank image of the same size
 		ImageResource outImage = new ImageResource(inImage.getWidth(), inImage.getHeight());
-		//for each pixel in outImage
-		for (Pixel pixel: outImage.pixels()) {
-			//look at the corresponding pixel in inImage
+		// for each pixel in outImage
+		for (Pixel pixel : outImage.pixels()) {
+			// look at the corresponding pixel in inImage
 			Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
-			//compute inPixel's red + inPixel's blue + inPixel's green
-			//divide that sum by 3 (call it average)
-			int average = (inPixel.getRed() + inPixel.getBlue() + inPixel.getGreen())/3;
-			//set pixel's red to average
+			// compute inPixel's red + inPixel's blue + inPixel's green
+			// divide that sum by 3 (call it average)
+			int average = (inPixel.getRed() + inPixel.getBlue() + inPixel.getGreen()) / 3;
+			// set pixel's red to average
 			pixel.setRed(average);
-			//set pixel's green to average
+			// set pixel's green to average
 			pixel.setGreen(average);
-			//set pixel's blue to average
+			// set pixel's blue to average
 			pixel.setBlue(average);
 		}
-		//outImage is your answer
+		// outImage is your answer
 		return outImage;
 	}
 
+	public void selectAndConvert() {
+		DirectoryResource dr = new DirectoryResource();
+		for (File f : dr.selectedFiles()) {
+			ImageResource inImage = new ImageResource(f);
+			ImageResource gray = makeGray(inImage);
+			gray.draw();
+		}
+	}
+
+	
 	public void testGray() {
 		ImageResource ir = new ImageResource();
 		ImageResource gray = makeGray(ir);
 		gray.draw();
 	}
+
+	public class ImageSaver {
+		public void doSave() {
+			DirectoryResource dr = new DirectoryResource();
+			for (File f : dr.selectedFiles()) {
+				ImageResource image = new ImageResource(f);
+				String fname = image.getFileName();
+				String newName = "copy-" + fname;
+				image.setFileName(newName);
+				image.draw();
+				image.save();
+			}
+		}
+	}
+
 }
