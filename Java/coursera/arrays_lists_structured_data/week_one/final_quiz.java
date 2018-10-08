20aa00da9508f293001bec8e23ff8034a6956131
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-10-08 19:40:11 -0400

    quiz finish weekone

---
+++ Java/coursera/arrays_lists_structured_data/week_one/final_quiz.java
@@ -0,0 +1,21 @@
+//1): Rpc ndj xbpvxct axut LXIWDJI iwt xcitgcti PCS rdbejitgh xc ndjg edrzti?
+//2): Xii twp duvodvz gqam EDBCWPB bcm qibzzimo VVY xwhxpbzzn dv gjcm kwxszb?
+//3): 
+if (i % 2 == 0)  {                 	
+     newChar = shiftedAlphabet1.charAt(idx);            
+}            
+else {                	
+     newChar = shiftedAlphabet2.charAt(idx);            
+}
+
+//4): 4
+//5): 7
+//6): 
+//7): The name of the Java Mascot is Duke. Woeoeee!
+//8): Duke Computer Science Department Overview
+//9): 17,4
+//10):key int should be private
+//11): private
+//12): It should not have a return type
+//13): blueblueblue is blue repeated
+//14): compile error
