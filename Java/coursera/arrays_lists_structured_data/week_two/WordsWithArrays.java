d750dd9f05af76ae2147ca2bad57f5633bb0368d
Junghoon Yoon <juhuyoon@yahoo.com>
2018-10-13 10:01:27 -0400

    arraylists

--- Java/coursera/arrays_lists_structured_data/week_two/WordsWithArrays.java
+++ Java/coursera/arrays_lists_structured_data/week_two/WordsWithArrays.java
@@ -8,7 +8,45 @@ public class WordsWithArrays { //StorageResource is used here as we don't know h
         myWords = new StorageResource();
     }
 
+    //read all the words and put it into storage of myWords in lowercase
     public void readWords() {
         myWords.clear();
+        FileResource resource = new FileResource();
+        for(String word: resource.words()) {
+            myWords.add(word.toLowerCase());
+        }
+    }
+
+    //Method that will check if what is passed already in the array
+    
+    public boolean contains(String[] list, String word, int number) {
+        for(int i = 0; i < number; i++) {
+            if(list[i].equals(word)) {
+                return true; //returns true if the word passed is already in the array
+            }
+        }
+        return false; 
+    }
+
+    //to note how many of the words are unique and to return that number of unique words when called upon
+    public int numberOfUniqueWords() {
+        int numStored = 0;
+        String[] words = new String[myWords.size()]; //creates a separate array that has all of the unique words. 
+                                    //myWords.size is kept open as we don't know how many words there might be
+        for(String s : myWords.data()){
+            if(! contains(words, s, numStored)) {
+                words[numStored] = s;
+                numStored++;
+            }
+        }
+        return numStored;
+    }
+
+    //tester method 
+    public void tester() {
+        readWords();
+        System.out.println("number of words read: " + myWords.size());
+        int unique = numberOfUniqueWords();
+        System.out.println("array count " + unique);
     }
 }
