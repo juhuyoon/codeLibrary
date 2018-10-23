f9fa7ffcdcac34124efbd7440a5610d31f9837a3
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-10-23 16:21:22 -0400

    moving to laptop

---
+++ Java/coursera/arrays_lists_structured_data/week_four/cipher.java
@@ -0,0 +1,18 @@
+//Vigenere cipher
+    //key is "word" (array of ints)
+    //use "mod" to wrap around 0,1,2,3,0,1,...
+
+//Message: Meet me At Dawn
+    //Key + 382438243824382
+    //Encrypted = Take through those keys at skip to the letters that are there
+
+//use slice string
+for(int i = 4; i < 30; i += 7) {
+    System.out.println(i);
+}
+
+//advice on writing sliceString:
+    //remember stringbuilder
+    //counting for loops can...
+        //start at any number
+        //count by any number
