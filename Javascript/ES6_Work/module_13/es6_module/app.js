8bbf163a618dbdc6a63a70cd40cc2da8fcc30123
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-12-14 19:12:46 -0500

    save these changed package.json files

--- Javascript/ES6_Work/module_13/es6_module/app.js
+++ Javascript/ES6_Work/module_13/es6_module/app.js
@@ -1,4 +1,13 @@
 import { uniq } from 'lodash';
 import insane from 'insane';
 import jsonp from 'jsonp';
+import { apiKey, url as website, sayHi } from './src/config';
 
+import User, { createURL, gravatar } from './src/user';
+
+const user = new User('Jung Test', 'juhuyoon@gmail.com', 'junghoonyoon.com');
+console.log(user);
+const profile = createURL(user.name);
+console.log(profile);
+const image = gravatar(user.email);
+console.log(image);
