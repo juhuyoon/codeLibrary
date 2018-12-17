2303095b564a438d7e851eaac6dcba0ef7fd8dd2
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-12-17 15:41:34 -0500

    changes

---
+++ Javascript/ES6_Work/Module_14/babel/app-compiled.js
@@ -0,0 +1,14 @@
+/* https://babeljs.io/docs/en/plugins/ */
+const {
+  x,
+  y,
+  ...z
+} = {
+  x: 1,
+  y: 2,
+  a: 3,
+  b: 4
+};
+const age = 100;
+const people = ['Wes', 'Kait'];
+const fullNames = people.map(name => `${name} Bos`);
