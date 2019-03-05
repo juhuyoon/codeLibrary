80a3a2e1136124cff579bbd7d501a6de6619a75d
Junghoon Yoon <juhuyoon@yahoo.com>
2019-03-04 21:03:30 -0500

    prac

---
+++ JavaAndroid/Arrays/src/com/juhuyoon/Main.java
@@ -0,0 +1,14 @@
+package com.juhuyoon;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] myIntArray = new int[10]; //this is not a regular variable, but an array.
+                            //10 elements max in the array of 0 ~ 9
+        myIntArray[5] = 50; //50 at position 6, since index starts at 0.
+        
+
+
+
+    }
+}
