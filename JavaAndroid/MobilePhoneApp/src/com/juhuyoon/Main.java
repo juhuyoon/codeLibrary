8375839d9037794311c4834b40a06a4dcf797852
Junghoon Yoon <juhuyoon@yahoo.com>
2019-03-08 01:30:01 -0500

    mobilephone

--- JavaAndroid/MobilePhoneApp/src/com/juhuyoon/Main.java
+++ JavaAndroid/MobilePhoneApp/src/com/juhuyoon/Main.java
@@ -1,8 +1,72 @@
 package com.juhuyoon;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone("303 404 5055");
 
     public static void main(String[] args) {
 
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while(!quit) {
+            System.out.println("\nEnter action: (6 to show available actions)");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(action) {
+                case 0:
+                    System.out.println("\nShutting down...");
+                    quit = true;
+                    break;
+                case 1:
+                    mobilePhone.printContacts();
+                    break;
+                case 2:
+                    addNewContact();
+                    break;
+                case 3:
+                    updateContacts();
+                    break;
+                case 4:
+                    removeContacts();
+                    break;
+                case 5:
+                    queryContacts();
+                    break;
+                case 6:
+                    printActions();
+                    break;
+
+            }
+
+        }
+    }
+
+    private static void startPhone() {
+        System.out.println("Starting phone...");
+    }
+
+    private static void addNewContact() {
+        System.out.println("Enter new contact name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phone = scanner.nextLine();
+        Contacts newContact = Contacts.createContact(name, phone);
+    }
+
+    private static void printActions() {
+        System.out.println("\nAvailable Actions: \npress");
+        System.out.println("0 - to shutdown\n" +
+                           "1 - to print contacts \n" +
+                           "2 - to add a new contact \n" +
+                           "3 - to update an existing contact \n" +
+                            "4 - to remove an existing contact \n" +
+                           "5 - query if an existing contact exists \n" +
+                           "6  - to print a list of available actions. "
+        );
+        System.out.println("Choose your actions");
     }
 }
--- JavaAndroid/MobilePhoneApp/src/com/juhuyoon/MobilePhone.java
+++ JavaAndroid/MobilePhoneApp/src/com/juhuyoon/MobilePhone.java
@@ -20,5 +20,57 @@ public class MobilePhone {
         return true;
     }
 
-    public int findContact()
+    public boolean updateContact(Contacts oldContact, Contacts newContact) {
+        int foundPosition = findContact(oldContact);
+        if(foundPosition < 0 ) {
+            System.out.println(oldContact.getName() + " was not found.");
+            return false;
+        }
+        this.myContacts.set(foundPosition, newContact);
+        System.out.println(oldContact.getName() + " , was replaced with " + newContact.getName());
+        return true;
+    }
+
+    public boolean removeContact(Contacts contact) {
+        int foundPosition = findContact(contact);
+        if(foundPosition < 0) {
+            System.out.println(contact.getName() + ", was not found.");
+            return false;
+        }
+        this.myContacts.remove(foundPosition);
+        System.out.println(contact.getName() + ", was deleted.");
+        return true;
+    }
+
+    public int findContact(Contacts contact) {
+        return this.myContacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName) {
+        for(int i = 0; i < myContacts.size(); i++ ){
+            Contacts contact = this.myContacts.get(i);
+            if(contact.getName().equals(contactName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String queryContact(Contacts contact) {
+        if(findContact(contact) >= 0 ) {
+            return contact.getName();
+        }
+        return null;
+    }
+
+    public void printContacts() {
+        System.out.println("Contact List");
+        System.out.println("Contact List");
+        for(int i = 0; i < this.myContacts.size(); i++) {
+            System.out.println((i+1) + " . " +
+                                this.myContacts.get(i).getName() + "-> "
+                                + this.myContacts.get(i).getPhoneNumber());
+        }
+
+    }
 }
