2430afacece8f1febc46d2f12b290b4ff729376d
Junghoon Yoon <juhuyoon@yahoo.com>
2019-03-09 12:38:54 -0500

    something autoboxing something something

--- JavaAndroid/AutoBoxingandUnboxing/src/com/juhuyoon/Main.java
+++ JavaAndroid/AutoBoxingandUnboxing/src/com/juhuyoon/Main.java
@@ -3,6 +3,23 @@ package com.juhuyoon;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+
+class IntClass {
+    private int myValue;
+
+    public IntClass(int myValue) {
+        this.myValue = myValue;
+    }
+
+    public int getMyValue() {
+        return myValue;
+    }
+
+    public void setMyValue(int myValue) {
+        this.myValue = myValue;
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,5 +30,8 @@ public class Main {
         strArrayList.add("Tim");
 
         //ArrayList<int> intArrayList = new ArrayList<int>(); Runs into issues here as int is a primitive type.
+        ArrayList<IntClass> intClassArrayList = new ArrayList<IntClass>();
+        intClassArrayList.add(new IntClass(54));
+        
     }
 }
