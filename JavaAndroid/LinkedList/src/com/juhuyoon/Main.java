27da33628106c8ccfea77af78c22a3f044490e77
Junghoon Yoon <juhuyoon@yahoo.com>
2019-03-11 23:31:52 -0400

    forgot to push basics

---
+++ JavaAndroid/LinkedList/src/com/juhuyoon/Main.java
@@ -0,0 +1,33 @@
+package com.juhuyoon;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //LinkedList determines the index's position in physical memory
+        //by address, taking index * 4 + 100 for the position.
+        //integer = 4 bytes of memory while double = 8 bytes.
+
+        Customer customer = new Customer("Tim", 54.96);
+        Customer anotherCustomer;
+        anotherCustomer = customer; //setting to customer class, overriding customer to anotherCustomer.
+        anotherCustomer.setBalance(12.18);
+        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for(int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1, 2);
+        for(int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+    }
+}
--- JavaAndroid/SimpleBanking/src/com/juhuyoon/Main.java
+++ JavaAndroid/SimpleBanking/src/com/juhuyoon/Main.java
@@ -3,6 +3,33 @@ package com.juhuyoon;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Bank bank = new Bank("Wells Fargo");
+
+        if(bank.addBranch("Adelaide")) {
+            System.out.println("Adelaide branch created");
+        };
+
+        bank.addCustomer("Adelaide", "Tim", 50.05);
+        bank.addCustomer("Adelaide", "Mike", 175.34);
+        bank.addCustomer("Adelaide", "Percy", 220.12);
+
+        bank.addBranch("Sydney");
+        bank.addCustomer("Sydney", "Bob", 150.54);
+
+        bank.addCustomerTransaction("Adelaide", "Tim", 44.22);
+        bank.addCustomerTransaction("Adelaide", "Tim", 12.44);
+        bank.addCustomerTransaction("Adelaide", "Tim", 1.65);
+
+        bank.listCustomers("Adelaide", true);
+        bank.listCustomers("Sydney", true);
+
+        if(!bank.addCustomer("Melbourne", "Brian", 5.53)) {
+            System.out.println("Error Melbourne branch does not exist");
+        }
+
+        if(!bank.addBranch("Adelaide")) {
+            System.out.println("Adelaide branch already exists");
+        }
+
     }
 }
