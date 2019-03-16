889bae741ba1067cd484e0c66d2e955ed21a40eb
Junghoon Yoon <juhuyoon@yahoo.com>
2019-03-16 12:40:27 -0400

    abstract classes

--- JavaAndroid/Inner-and-Abstract-Classes-&-Interfaces-Abstract-Classes-Part-1-Source-code/src/com/timbuchalka/Main.java
+++ JavaAndroid/Inner-and-Abstract-Classes-&-Interfaces-Abstract-Classes-Part-1-Source-code/src/com/timbuchalka/Main.java
@@ -3,6 +3,8 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
+        //abstract classes define methods, but do not provide implementation of the methods.
+        //implementation by the inheritance of the classes from the abstract classes.
 	    Dog dog = new Dog("Yorkie");
         dog.breathe();
         dog.eat();
