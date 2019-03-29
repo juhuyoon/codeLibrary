767371f49b88f2a7eb57a27d483f449604391a01
Junghoon Yoon <juhuyoon@yahoo.com>
2019-03-28 20:48:54 -0400

    what did i do

--- Javascript/HackerRank10JS.js
+++ Javascript/HackerRank10JS.js
@@ -584,3 +584,13 @@ function sides(literals, ...expressions) {
 
     return ([s2,s1]);
 }
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    return nums.map(n => n = (n % 2 == 0) ? n * 2 : n * 3);
+}
