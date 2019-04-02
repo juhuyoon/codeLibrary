fe5e4601605a1ed41e2d45850c31af08cdb31e74
Junghoon Yoon <juhuyoon@yahoo.com>
2019-04-02 18:12:33 -0400

    hackerrank

--- Javascript/interviewprepkit.js
+++ Javascript/interviewprepkit.js
@@ -13,7 +13,7 @@ function sockMerchant(n, ar) {
     var h = {};
     
     for (var i = 0; i < n; i++) {
-        if (ar[i] in h) {
+        if (ar[i] in h) { //for in loop 
             h[ar[i]] = h[ar[i]] + 1;
         } else {
             h[ar[i]] = 1;
@@ -27,3 +27,16 @@ function sockMerchant(n, ar) {
 
     return count;
 }
+
+/* 
+Emma is playing a new mobile game that starts with consecutively numbered clouds. 
+Some of the clouds are thunderheads and others are cumulus. 
+She can jump on any cumulus cloud having a number that is equal to the number of current clouds plus 1 or 2. 
+She must avoid the thunderheads. Determine the minimum number of jumps it will take Emma to jump from her starting position to the last cloud.
+ It is always possible to win the game.
+
+For each game, Emma will get an array of clouds numbered 0 if they are safe or 1 if they must be avoided. 
+For example, c = [0, 1, 0, 0, 0, 1, 0] indexed from 0...6. 
+The number on each cloud is its index in the list so she must avoid the clouds are indexes 1 and 5. 
+She could follow the following two paths: 0 -> 2 -> 4 -> 6 or 0 -> 2 -> 3 -> 4 -> 6.
+ The first path takes 3 jumps while the second takes 4.*/
