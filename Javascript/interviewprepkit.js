75beba41635838562d1c0be595269bf2664f8163
Junghoon Yoon <juhuyoon@yahoo.com>
2019-04-08 05:54:25 -0400

    new year chaos finished

--- Javascript/interviewprepkit.js
+++ Javascript/interviewprepkit.js
@@ -205,11 +205,40 @@ console.log(arr6); //[{a: 5}, {b:2}, {c:3}];
 /* 
 It's New Year's Day and everyone's in line for the Wonderland rollercoaster ride! There are a number of people queued up, and each person wears a sticker indicating their initial position in the queue. Initial positions increment by 1 from 1 at the front of the line to n at the back.
 
-Any person in the queue can bribe the person directly in front of them to swap positions. If two people swap positions, they still wear the same sticker denoting their original places in line. One person can bribe at most two others. For example, if n = 8 and Person 5 bribes Person 5, the queue will look like this: 1, 2, 3, 5, 4, 6, 7, 8.
+Any person in the queue can bribe the person directly in front of them to swap positions. If two people swap positions, they still wear the same sticker denoting their original places in line. One person can bribe at most two others. For example, if n = 8 and Person 5 bribes Person 4, the queue will look like this: 1, 2, 3, 5, 4, 6, 7, 8.
 
 Fascinated by this chaotic queue, you decide you must know the minimum number of bribes that took place to get the queue into its current state!
 */
 
+//Keep track of each bribe and go through the sequencing
 const minimumBribes = (q) => {
-    
+    let bribes = 0;
+    let swapped = true;
+    let tooChaotic = false;
+    let firstRun = true;
+
+    while(swapped === true) {
+        swapped = false;
+        for(let i = 0; i < q.length; i++) {
+            if(q[i] - 1 > 3 && firstRun) {
+                tooChaotic = true;
+            }
+
+            if(q[i] && q[i+1] && q[i] > q[i+1]) {
+                let temp = q[i];
+                q[i] = q[i+1];
+                q[i+1] = temp;
+                swapped = true;
+                bribes++;
+            }
+        }
+        firstRun = false;
+    }
+
+    if(tooChaotic) {
+        console.log(bribes);
+        console.log("Too chaotic");
+    } else {
+        console.log(bribes);
+    }
 }
