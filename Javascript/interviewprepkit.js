56d69dd58409a82dc2702842f44a359911f19793
Junghoon Yoon <juhuyoon@yahoo.com>
2019-04-08 08:10:23 -0400

    ?

--- Javascript/interviewprepkit.js
+++ Javascript/interviewprepkit.js
@@ -294,5 +294,12 @@ const fibonacci = n => {
     for(let i = 2; i < n + 1; i++) {
         arr.push(arr[i-2] + arr[i-1]);
     }
-    return arr[n]
+    return arr[n];
+}
+
+function fibRecursive(n) {
+    if(n < 2) {
+        return n
+    }
+    return fibRecursive(n - 1) + fibRecursive(n - 2);
 }
