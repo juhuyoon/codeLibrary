1637a638fa71bd6c949b85d44a378a72f9de1020
Junghoon Yoon <juhuyoon@yahoo.com>
2019-04-18 05:11:44 -0400

    instructxions git

--- Javascript/interviewprepkit.js
+++ Javascript/interviewprepkit.js
@@ -353,5 +353,12 @@ function twoStrings(s1, s2) {
         return result;
 }
 
-//Bubble Sort Sorting
+//Mark and Toys
+/* Mark and Jane are very happy after having their first child. 
+Their son loves toys, so Mark wants to buy
+There are a number of different toys lying in front of him, tagged with their prices. Mark has only a certain amount to spend, and he wants to maximize the number of toys he buys with this money.*/
+
+//organize in ascending order,
+//subtract prices from total money  before no more money
+//won't buy others if we can't buy highest priced one.
 
