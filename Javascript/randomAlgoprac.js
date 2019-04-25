9e02f17e66cae4960115c8d601005d3ce7156baf
Junghoon Yoon <juhuyoon@yahoo.com>
2019-04-25 18:55:34 -0400

    update array with no duplicate

--- Javascript/randomAlgoprac.js
+++ Javascript/randomAlgoprac.js
@@ -903,3 +903,27 @@ const getDay = () => {
         break;
     }
 }
+
+/* 
+Write a function that takes in an array of integers and the array with duplicates removed
+
+Ex:
+Input: [1,2,2,3]
+Output: [1,2,3]
+
+Input: [4,5,4,4,7,5]
+Output: [4,5,7]
+
+Input: [1,2,3,5]
+Output: [1,2,3,5]
+
+*/
+
+const redUpd = (arr) => {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(!newArr.includes(arr[i])){
+            newArr.push(arr[i]);
+        }
+    }
+}
