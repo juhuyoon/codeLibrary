977f573beef7adf1cf0474e7cb52a077bbfd0a3a
Junghoon Yoon <juhuyoon@yahoo.com>
2019-04-27 09:45:55 -0400

    randomPass

--- Javascript/randomAlgoprac.js
+++ Javascript/randomAlgoprac.js
@@ -927,3 +927,44 @@ const redUpd = (arr) => {
         }
     }
 }
+
+/* 
+Write a function that takes a string as a parameter and determines if it is a valid password with the following constraints:
+
+1. It must be at least 8 characters long
+2. It must contain at least 1 capital letter
+3. It must contain at least 1 lower case letter
+
+Output true if the string is a valid password or false otherwise.
+
+Ex:
+Input: RexTheDog
+Output: true
+
+Input: rexthedog
+Output: false
+
+Input: REXTHEDOG
+Output: false
+
+Input: Dog
+Output: false
+*/
+
+const rightPass = (str) => {
+    let isUpper = false;
+    let isLower = false;
+    if(str.length < 8) {
+        return false
+    }
+
+    for(let i = 0; i < str.length; i++) {
+        if(password[i].toLowerCase === password[i]) {
+            return true;
+        } 
+        if(password[i].toUpperCase === password[i]) {
+            return true;
+        }
+    }
+    return isUpper && isLower;
+}
