8a513ad5baf884b1760f09d5d565ab3ee882e356
Junghoon Yoon <juhuyoon@yahoo.com>
2019-05-04 13:10:58 -0400

    callback prac

--- Javascript/Randomfunc.js
+++ Javascript/Randomfunc.js
@@ -307,3 +307,48 @@ let data = [
         });
 
         //ages = 84;
+
+        var fs = require("fs");
+
+function logBefore(func, message) {
+  console.log(message);
+
+  func();
+}
+
+function runIf(func, bool) {
+  if (bool) {
+    func();
+  }
+}
+
+function wrap(func, value) {
+  return function() {
+    return func(value);
+  };
+}
+
+// Callbacks appear in familiar places, like fs.writeFile.
+fs.writeFile("log.txt", "Log message!", function(err) {
+
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("File saved!");
+
+});
+
+// Alternatively, we can assign our function to a variable,
+// and pass it by name.
+var writeFileCallback = function(err) {
+
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("File saved!");
+
+};
+
+fs.writeFile("log.txt", "Log message!", writeFileCallback);
