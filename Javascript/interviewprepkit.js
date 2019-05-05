d18db8d97d4fbd67ae099bf3cd7311211bfd3664
Junghoon Yoon <juhuyoon@yahoo.com>
2019-05-04 22:25:13 -0400

    whiteboard

--- Javascript/interviewprepkit.js
+++ Javascript/interviewprepkit.js
@@ -457,3 +457,57 @@ const isInArray = (arr, elem) => {
         }
     }
 }
+
+//API endpoint question:
+// 'https://api.agency.gov/data/fiscal?format=json';
+
+//example response:
+// {
+//     results: [{
+//         name: 'EPA',
+//         outlay_fy: '2019',
+//         outlay_fq: '1',
+//         outlay_amt: '20000',
+//         budget: '35000',
+//         contact:'Zinka, Ryan(LOL)'
+//     }, 
+//     {
+//         name: 'USCIS',
+//         outlay_fy:'2018',
+//         outlay_fq: '3', 
+//         outlay_amt: '100000',
+//         budget: '15000',
+//         contact: 'whoever'
+//     },
+//     ...
+//     ]
+// }
+
+//using node, write an application that will retrieve the agency data at the endpoint above,
+//and will log QUARTERLY outlay totals for the FISCAL year (2018).
+
+//example output:
+    //Q1-2018: 52,000,000.9162746
+    //Q2-2018: 61,000,000.105219
+    //Q3-2018: 120,586,342.013672
+    //Q4-2018: 2,467,826,422.6176829
+
+var request = require('request');
+request('https://api.agency.gov/data/fiscal?format=json', function(err, res, body) {
+    console.log(res.body);
+    console.log(body);
+    console.log(res.body.data);
+
+    let year18 = [];
+    let quarter18 = [0,0,0,0];
+    for(let i = 0; i < res.body.length; i++) {
+        if(res.body.data.outlay_fy === '2018') {
+            res.body.data.push(year18);
+        }
+    }
+    for(let i = 0; i < year18.length; i++) {
+        if(outlay_fq === '1') {
+            parseFloat(year18[i].push(quarter18[0]));
+        }
+    }
+});
