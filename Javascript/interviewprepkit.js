d30dc5439ab2cbed3a2ff5bd39438c40a739921e
Junghoon Yoon <juhuyoon@yahoo.com>
2019-05-09 18:01:54 -0400

    work arounds

--- Javascript/interviewprepkit.js
+++ Javascript/interviewprepkit.js
@@ -538,3 +538,32 @@ const arrDouble = (arr) => {
     return result;
 }
 
+/* 
+Write a function that takes an array of numbers and a function as parameters. 
+The function parameter should do something to a number (increment, double, decrement, etc) and return the result.
+Your function should return the array that results from applying the function parameter to each element in the number array. 
+
+Ex:
+Input: [1,2,3]  function(num){return num * 2*}
+Output: [2,4,6]
+
+Input: [1,2,3]  function(num){return num + 1}
+Output: [2,3,4]
+
+Input: [1,2,3] function(num) {return num /2}
+Output: [.5, 1. 1.5]
+
+Input: [1,2,3] function(num) {return num - 2}
+Output: [-1, 0, 1]
+*/
+
+
+const arrFun = (arr, func) => {
+    let resArr = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        var resArr = func(arr[i]);
+        resArr.push(resArr);
+    }
+    return resArr;
+}
