fa3bc699e8e66f7a7adddf0d7caddb952b755e67
Junghoon Yoon <juhuyoon@yahoo.com>
2019-05-11 12:54:52 -0400

    work

--- Javascript/interviewprepkit.js
+++ Javascript/interviewprepkit.js
@@ -567,3 +567,35 @@ const arrFun = (arr, func) => {
     }
     return resArr;
 }
+
+/* 
+Write a function that takes an array of numbers and a function as parameters.
+The function parameter should return true if the input meets a certain condition or false otherwise. 
+Your function should return true if the function parameter returns true for _any_ of the array elements in the array parameter or false otherwise. 
+
+Ex:
+
+Input: [1,2,3]  function(num){return num === 2}
+Output: true
+
+Input: [1,5,3]  function(num){return num === 2}
+Output: false
+
+Input: [1,2,3]  function(num){return num % 2 === 0}
+Output: true
+
+Input: [1,5,3]  function(num){return num % 2 === 0}
+Output: false
+*/
+
+const anyTrue = (arr, func) => {
+    let anyTrue = false;
+
+    for(let i = 0; i < arr.length; i++) {
+        didFunc = func(arr[i]);
+        if(didFunc) {
+            anyTrue = didFunc; //if didFunc is true, then anyTrue = true
+        }
+    }
+    return anyTrue;
+}
