c53c356afe54330e14a587410b840d75e29a8a35
Junghoon Yoon <juhuyoon@yahoo.com>
2019-05-14 18:32:53 -0400

    return input correct

--- Javascript/interviewprepkit.js
+++ Javascript/interviewprepkit.js
@@ -599,3 +599,34 @@ const anyTrue = (arr, func) => {
     }
     return anyTrue;
 }
+
+
+/* 
+Write a function that takes an array of numbers and a function as parameters. 
+The function parameter should return true if the input meets a certain condition or false otherwise.
+Your function should run the function parameter on every element in the array parameter and ,
+if it returns true, add the element to a new array.
+Return the new array. 
+
+Ex:
+
+Input: [1,2,3]  function(num){return num === 2}
+Output: [2]
+
+Input: [1,2,3,4,5,6]  function(num){return num % 2 === 0}
+Output: [2,4,6]
+
+Input: [1,2,3,4,5,6]  function(num){return num > 3}
+Output: [4,5,6]
+*/
+
+let checkTrue = (arr, func) => {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i ++) {
+        let newFunc = func(arr[i]);
+        if(newFunc) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
