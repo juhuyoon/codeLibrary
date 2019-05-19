03fc5decd3b7bc253fbd84ab0b156608f360c4e1
Junghoon Yoon <juhuyoon@yahoo.com>
2019-05-18 22:11:14 -0400

    array

---
+++ Javascript/Daily Coding Problem/p2.js
@@ -0,0 +1,26 @@
+/* 
+This problem was asked by Uber.
+
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+*/
+
+const p2 = (list) => {
+    let product = 1;
+
+    for(let i = 0; i < list.length; i ++) {
+        product *= list[i];
+    }
+    for(let i = 0; i < list.length; i++) {
+        //divide by the current index then put into list. 
+        list[i] = product / list[i];
+    }
+    
+    return list;
+}
+
+
+console.log(p2([1, 2, 3, 4, 5]));
