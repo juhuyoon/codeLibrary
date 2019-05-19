70b1804362bdd466a2e733ab5b80751534bb7bec
Junghoon Yoon <juhuyoon@yahoo.com>
2019-05-19 18:14:37 -0400

    p4

---
+++ Javascript/Daily Coding Problem/p4.js
@@ -0,0 +1,12 @@
+/* 
+This problem was asked by Stripe.
+
+Given an array of integers, find the first missing positive integer in linear time and constant space. 
+In other words, find the lowest positive integer that does not exist in the array. 
+The array can contain duplicates and negative numbers as well.
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3. 
+You can modify the input array in-place.
+*/
+
+//Linear time and constant space. O(n) time and O(n) space.
+
