46d5aa965fd93f1751985d1b745cfe26b31c81fe
Junghoon Yoon <juhuyoon@yahoo.com>
2019-05-20 16:23:44 -0400

    fb decoder

--- Javascript/Daily Coding Problem/p6.js
+++ Javascript/Daily Coding Problem/p6.js
@@ -9,5 +9,13 @@ You can assume that the messages are decodable. For example, '001' is not allowe
 /* 
 Decoder... 
 Does the decoded message have to be 3 letters, or it can be any. Count number of ways it can be decoded...
-
+convert the values to string, then split them based on their index and push to empty array and return it. 
 */
+
+const e1 = 111;
+const e2 = 12345;
+const e3 = 4;
+
+const decode = (message) => {
+    
+}
