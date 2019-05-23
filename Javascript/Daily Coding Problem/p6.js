17d6ef2a7e97e1e8b366ff7674d6660d56057631
Junghoon Yoon <juhuyoon@yahoo.com>
2019-05-22 20:43:59 -0400

    logic

--- Javascript/Daily Coding Problem/p6.js
+++ Javascript/Daily Coding Problem/p6.js
@@ -10,12 +10,22 @@ You can assume that the messages are decodable. For example, '001' is not allowe
 Decoder... 
 Does the decoded message have to be 3 letters, or it can be any. Count number of ways it can be decoded...
 convert the values to string, then split them based on their index and push to empty array and return it. 
+parameter (1 ~ 26)
+loop through, go through each iteration possible, 
 */
 
 const e1 = 111;
 const e2 = 12345;
 const e3 = 4;
 
-const decode = (message) => {
-    
+const decodeCount = (message) => {
+    message = message.toString();
+
+    const array1 = [];
+    const array2 = [];
+    for(let i = 0; i < message.length; i++) {
+        if(message.charAt(i+1)) {
+            array1.push(message.charAt(i) + message.charAt(i+1));
+        }
+    }
 }
