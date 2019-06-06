8e5e490989167f2c3b307af19d7d6c72eed546a9
Junghoon Yoon <juhuyoon@yahoo.com>
2019-06-05 22:16:59 -0400

    nonadjsum

--- Javascript/interviewprepkit.js
+++ Javascript/interviewprepkit.js
@@ -678,3 +678,59 @@ var singleWord = function (str){
   
     return newStr;
   }
+
+  /*
+This problem was asked by Amazon.
+
+Given an array of numbers, find the maximum sum of any contiguous subarray of the array.
+
+For example, given the array [34, -50, 42, 14, -5, 86], the maximum sum would be 137, since we would take elements 42, 14, -5, and 86.
+
+Given the array [-5, -1, -8, -9], the maximum sum would be 0, since we would not take any elements.
+
+Do this in O(N) time.
+*/
+
+const maxSumArray = (arr) => {
+    let prev = 0;
+    let maxNum = -Number.MAX_VALUE;
+
+    for(let i = 0; i < arr.length; i++) {
+        prev = Math.max(prev + arr[i], arr[i]);
+        maxNum = Math.max(maxNum, prev);
+    }
+    return maxNum;
+}
+
+/* 
+This problem was asked by Airbnb.
+Given a list of integers, write a function that returns the largest sum of non-adjacent numbers.
+Numbers can be 0 or negative. For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, 
+and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+
+Follow-up: Can you do this in O(N) time and constant space?
+*/
+
+//Probably doesn't work...?
+// const nonAdjSum = (arr) => {
+//     let prev = 0;
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//         prev = (arr[i] + arr[i+2]);
+//         sum += sum;
+//     }
+//     return sum;
+// }
+
+const nonAdjSum = (arr) => {
+    let incl = 0;
+    let excl = 0;
+    let new_excl;
+    arr.forEach( (el) => {
+        new_excl = excl > incl ? excl : incl;
+        incl = excl + el;
+        excl = new_excl;
+    })
+    return excl > incl ? excl: incl;
+}
+
--- Javascript/randomAlgoprac.js
+++ Javascript/randomAlgoprac.js
@@ -1004,23 +1004,3 @@ const hasUpperLower = (password) => {
     })
 }
 
-/*
-This problem was asked by Amazon.
-
-Given an array of numbers, find the maximum sum of any contiguous subarray of the array.
-
-For example, given the array [34, -50, 42, 14, -5, 86], the maximum sum would be 137, since we would take elements 42, 14, -5, and 86.
-
-Given the array [-5, -1, -8, -9], the maximum sum would be 0, since we would not take any elements.
-
-Do this in O(N) time.
-*/
-
-const maxSumArray = (arr) => {
-    let prev = 0;
-    let maxNum = -Number.MAX_VALUE;
-
-    for(let i = 0; i < arr.length; i++) {
-        prev = Math.max(prev + )
-    }
-}
