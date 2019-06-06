b629ee52e7a2936b4c78cb256d7a04e4d2f625b1
Junghoon Yoon <juhuyoon@yahoo.com>
2019-06-06 18:47:37 -0400

    twosum

--- Javascript/interviewprepkit.js
+++ Javascript/interviewprepkit.js
@@ -734,3 +734,29 @@ const nonAdjSum = (arr) => {
     return excl > incl ? excl: incl;
 }
 
+/* 
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+*/
+
+//Runtime: O(n^2)
+
+const two_sum = (list, k) => {
+    for(let i = 0; i < list.length; i++) {
+        for(let j = 0; j=list.length; j++) {
+            if((i != j) && (list[i] + list[j] === k)) {
+                return true;
+            }
+            else {
+                return false; 
+            }
+        }
+    }
+}
+
+//Runtime O(n)
